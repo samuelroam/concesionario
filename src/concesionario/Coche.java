9a8a843da33b4c4109aec9fdcdcc1f339bbb54e0
ik013043z1 <ik013043z1@hz341930-206-11.H013043.NET>
2019-05-15 13:13:42 +0200

    clases con herencia

--- src/concesionario/Coche.java
+++ src/concesionario/Coche.java
@@ -1,61 +1,14 @@
 package concesionario;
 
-public class Coche {
+public class Coche extends Vehiculo {
 
-	private String matricula;
-	private int numBastidor;
-	private String color;
-	private String colorNuevo;
-	private int numAsientos;
-	private float precio;
-	private int serie;
+	
 	private int numPuertas;
 	private int capacidadMaletero;
 	private String tipo;
 	
 	
-	public String getMatricula() {
-		return matricula;
-	}
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
-	public int getNumBastidor() {
-		return numBastidor;
-	}
-	public void setNumBastidor(int numBastidor) {
-		this.numBastidor = numBastidor;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	public String getColorNuevo() {
-		return colorNuevo;
-	}
-	public void setColorNuevo(String colorNuevo) {
-		this.colorNuevo = colorNuevo;
-	}
-	public int getNumAsientos() {
-		return numAsientos;
-	}
-	public void setNumAsientos(int numAsientos) {
-		this.numAsientos = numAsientos;
-	}
-	public float getPrecio() {
-		return precio;
-	}
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
-	public int getSerie() {
-		return serie;
-	}
-	public void setSerie(int serie) {
-		this.serie = serie;
-	}
+	
 	public int getNumPuertas() {
 		return numPuertas;
 	}
---
+++ src/concesionario/Vehiculo.java
@@ -0,0 +1,56 @@
+package concesionario;
+
+public class Vehiculo {
+	private String matricula;
+	private int numBastidor;
+	private String color;
+	private String colorNuevo;
+	private int numAsientos;
+	private float precio;
+	private int serie;
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public int getNumBastidor() {
+		return numBastidor;
+	}
+	public void setNumBastidor(int numBastidor) {
+		this.numBastidor = numBastidor;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColorNuevo() {
+		return colorNuevo;
+	}
+	public void setColorNuevo(String colorNuevo) {
+		this.colorNuevo = colorNuevo;
+	}
+	public int getNumAsientos() {
+		return numAsientos;
+	}
+	public void setNumAsientos(int numAsientos) {
+		this.numAsientos = numAsientos;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+
+}
