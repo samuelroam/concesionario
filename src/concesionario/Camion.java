4552769d0b1a65167ee30e52cf186dbb71071dcc
ik013043z1 <ik013043z1@hz341930-206-11.H013043.NET>
2019-05-15 13:11:51 +0200

    Generados los objetos coche y camion

---
+++ src/concesionario/Camion.java
@@ -0,0 +1,77 @@
+package concesionario;
+
+public class Camion {
+	private String matricula;
+	private int numBastidor;
+	private String color;
+	private String colorNuevo;
+	private int numAsientos;
+	private float precio;
+	private int serie;
+	private int carga;
+	private String tipoMercancia;
+	private String tipo;
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public int getNumBastidor() {
+		return numBastidor;
+	}
+	public void setNumBastidor(int numBastidor) {
+		this.numBastidor = numBastidor;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColorNuevo() {
+		return colorNuevo;
+	}
+	public void setColorNuevo(String colorNuevo) {
+		this.colorNuevo = colorNuevo;
+	}
+	public int getNumAsientos() {
+		return numAsientos;
+	}
+	public void setNumAsientos(int numAsientos) {
+		this.numAsientos = numAsientos;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	public int getCarga() {
+		return carga;
+	}
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+	public String getTipoMercancia() {
+		return tipoMercancia;
+	}
+	public void setTipoMercancia(String tipoMercancia) {
+		this.tipoMercancia = tipoMercancia;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ src/concesionario/Coche.java
@@ -0,0 +1,77 @@
+package concesionario;
+
+public class Coche {
+
+	private String matricula;
+	private int numBastidor;
+	private String color;
+	private String colorNuevo;
+	private int numAsientos;
+	private float precio;
+	private int serie;
+	private int numPuertas;
+	private int capacidadMaletero;
+	private String tipo;
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public int getNumBastidor() {
+		return numBastidor;
+	}
+	public void setNumBastidor(int numBastidor) {
+		this.numBastidor = numBastidor;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColorNuevo() {
+		return colorNuevo;
+	}
+	public void setColorNuevo(String colorNuevo) {
+		this.colorNuevo = colorNuevo;
+	}
+	public int getNumAsientos() {
+		return numAsientos;
+	}
+	public void setNumAsientos(int numAsientos) {
+		this.numAsientos = numAsientos;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	public int getNumPuertas() {
+		return numPuertas;
+	}
+	public void setNumPuertas(int numPuertas) {
+		this.numPuertas = numPuertas;
+	}
+	public int getCapacidadMaletero() {
+		return capacidadMaletero;
+	}
+	public void setCapacidadMaletero(int capacidadMaletero) {
+		this.capacidadMaletero = capacidadMaletero;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
